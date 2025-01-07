e5039bc86dac7246ce223b18b7726bcfe8ebf256
Omar Behri <omarbehri1@gmail.com>
2025-01-08 00:13:56 +0100

    added company offers

--- src/pages/student/interviews.js
+++ src/pages/student/interviews.js
@@ -65,7 +65,7 @@ export default function StudentInterviews() {
   };
 
   const handleJoinMeeting = (meetingLink) => {
-    window.open(meetingLink, '_blank');
+    window.location.href = meetingLink;
   };
 
   if (loading) {
