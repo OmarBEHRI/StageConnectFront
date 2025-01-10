e509cc6b8e64b5166937685a8e2e5a796d3dbe7d
BAKKALI Douae <bakkalidouae75@gmail.com>
2025-01-10 04:56:03 +0100

    changed the port for oracle db 8080

--- src/axiosInstance/axiosInstance.js
+++ src/axiosInstance/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8085', // Replace with your API's base URL
+  baseURL: 'http://localhost:8080', // Replace with your API's base URL
   timeout: 5000, // Optional: Set a timeout (in ms)
   headers: {
     'Content-Type': 'application/json', // Default content type
