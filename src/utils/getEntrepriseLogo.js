7ce2430ce542db2f43943539308c8eecc03b341b
Omar Behri <omarbehri1@gmail.com>
2025-01-14 19:44:35 +0100

    added ttestation and convention to tables

--- src/utils/getEntrepriseLogo.js
+++ src/utils/getEntrepriseLogo.js
@@ -2,11 +2,13 @@ import axiosInstance from '@/axiosInstance/axiosInstance';
 
 async function getEntrepriseLogoUrl(entrepriseId) {
   try {
+    console.log(`Entreprise Id collected for  getting logo is: ${entrepriseId}`)
     const logoResponse = await axiosInstance.get(`/api/entreprises/download/${entrepriseId}/logo`, {
       responseType: 'blob',
     });
     if (logoResponse.data.size > 0) {
       const logoBlob = new Blob([logoResponse.data], { type: 'image/jpeg' });
+      console.log(`URL collected for image is: ${URL.createObjectURL(logoBlob)}`);
       return URL.createObjectURL(logoBlob);
     }
   } catch (error) {
