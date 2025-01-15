765f48afaffba25048319f6c5f498dd8d17d2296
Omar Behri <omarbehri1@gmail.com>
2025-01-15 08:13:37 +0100

    fixed table

--- src/pages/etudiant/stages.js
+++ src/pages/etudiant/stages.js
@@ -129,7 +129,7 @@ export default function StudentInternships() {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {internships.map((internship) => {
-            const entreprise = getEntreprise(internship.offerId); // Get entreprise details
+            const entreprise = getEntreprise(internship.offreId); // Get entreprise details
             return (
               <Card
                 key={internship.idStage}
